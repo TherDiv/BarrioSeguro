08ecd24bed3a8871fcd99000c3d7ee8d03d810c1
Hecze <hectorzerrillo85@gmail.com>
2024-03-07 16:04:53 -0500

    calculadora de presupuesto arreglada

--- app/BudgeCalculator/page.tsx
+++ app/BudgeCalculator/page.tsx
@@ -170,7 +170,6 @@ export default function BudgeCalculatorPage() {
       <div className="flex flex-col gap-2">
         <Modal
           isOpen={isOpen}
-          placement={modalPlacement}
           onOpenChange={onOpenChange}
         >
           <ModalContent>
