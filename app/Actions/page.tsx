8a1b4dfbec610e8f65b077b5c688ec8a83175a3f
JimSegovia <textilesarvaljim@gmail.com>
2024-06-29 18:26:37 -0500

    Actualizar Actions

--- app/Actions/page.tsx
+++ app/Actions/page.tsx
@@ -5,6 +5,7 @@ import { Action } from '@/components/Action';
 import { BsHouses } from "react-icons/bs";
 import { BsHouseUp } from "react-icons/bs";
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function ActionPage() {
   const router = useRouter();
@@ -22,13 +23,17 @@ export default function ActionPage() {
 
   return (
     <div className="flex justify-center flex-col gap-12">
-      <p className='font-bold text-2xl mb-16'>¿Qué acción desea realizar?</p>
+      <p className='font-bold text-2xl mb-1 text-black'>¿Qué acción desea realizar?</p>
       <Link href="/GenerateCode">
         <Action text={'Crear una asociación de viviendas'} icon={BsHouses} />
       </Link>
       <Link href={"/InsertCode"}>
         <Action text={'Ingresar como vecino de una asociación de viviendas'} icon={BsHouseUp} />
       </Link>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '60px' }}>
+        <Image src={"/logo1.png"} width={250} height={250} alt={'xd'} />
+      </div>
     </div>
   );
 }
+
