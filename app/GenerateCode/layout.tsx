3dcd98943abc0eca1126efa54c7e19c981fc0b5b
JimSegovia <textilesarvaljim@gmail.com>
2024-06-29 18:31:36 -0500

    Added: Waves

--- app/GenerateCode/layout.tsx
+++ app/GenerateCode/layout.tsx
@@ -4,8 +4,11 @@ export default function AboutLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
+		<section className="relative flex flex-col items-center justify-center gap-4 py-8 md:py-10 w-full min-h-screen">
+			{/* Ajustamos el tamaño de fondo y posición para que la imagen se alargue y se mantenga en la parte superior */}
+			<div className="absolute top-0 left-0 w-full h-1/5 bg-[url('/wave1.svg')] bg-[length:auto] bg-top bg-repeat-x z-0 opacity-40" />
+			<div className="absolute top-0 left-0 w-full h-1/5 bg-[url('/wave2.svg')] bg-[length:auto] bg-top bg-repeat-x z-0 opacity-60" />
+			<div className="relative z-10 inline-block max-w-lg text-center">
 				{children}
 			</div>
 		</section>
