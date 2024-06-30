b98a1d725308d19937b7a47f1475fe62fed3a298
Esther <esther.ticlla@unmsm.edu.pe>
2024-06-30 00:04:19 -0500

    Update AddVecino

--- app/Asociation/[id]/AddVecino/page.tsx
+++ app/Asociation/[id]/AddVecino/page.tsx
@@ -1,14 +1,47 @@
-"use client"
-import { Header } from '@/components/Header';
+"use client";
+import React from 'react';
+import Header from '@/components/Header';
 import ElegirTipoVecino from '@/components/ElegirTipoVecino';
+import { FaUserFriends, FaUserTie, FaDollarSign, FaShieldAlt } from 'react-icons/fa';
 
-export default function AddVecino() {
+const AddVecino = () => {
+  const handleButtonClick = (type: string) => {
+    console.log(`Button clicked: ${type}`);
+    // Aquí puedes manejar la navegación o cualquier otra acción al hacer clic en el botón
+  };
 
   return (
     <>
       <Header text={'Elija el tipo de Vecino'} />
-      <ElegirTipoVecino />
+      <div className="flex flex-wrap justify-center gap-4 mt-4">
+        <ElegirTipoVecino
+          type="resident"
+          label="Residente"
+          icon={<FaUserFriends />}
+          onClick={handleButtonClick}
+        />
+        <ElegirTipoVecino
+          type="secretary"
+          label="Secretario"
+          icon={<FaUserTie />}
+          onClick={handleButtonClick}
+        />
+        <ElegirTipoVecino
+          type="treasurer"
+          label="Tesorero"
+          icon={<FaDollarSign />}
+          onClick={handleButtonClick}
+        />
+        <ElegirTipoVecino
+          type="security"
+          label="Vigilante"
+          icon={<FaShieldAlt />}
+          onClick={handleButtonClick}
+        />
+      </div>
     </>
-
   );
-}
+};
+
+export default AddVecino;
+
