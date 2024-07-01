dbfb92874a3a1cadddb5e7645cb9f60c7a458fe7
Hecze <hectorzerrillo85@gmail.com>
2024-06-30 22:54:42 -0500

    TIPO DE VECINO

--- components/ModificarButtons.tsx
+++ components/ModificarButtons.tsx
@@ -1,11 +1,14 @@
 import { Button, Link } from '@nextui-org/react';
 import { FaUserPlus, FaCog } from 'react-icons/fa';
+import { useEffect } from 'react';
 
 export default function ModificarButton() {
+    const associationCode = localStorage.getItem('associationId');
+
     return (
         <div className="fixed bottom-0 left-0 w-full bg-gradient-to-r from-blue-500 to-blue-700  p-4 ">
             <div className="flex flex-col items-center">
-                <Link href='/AddVecino' className='gap-4'>
+            <Link href={`/Asociation/${associationCode}/AddVecino`} className='gap-4'>
 
                     <Button className="mb-2 w-80  flex items-center justify-center gap-2  ">
                         <FaUserPlus />
@@ -13,7 +16,7 @@ export default function ModificarButton() {
 
                     </Button>
                 </Link>
-                <Link href='/Settings' className='gap-4'>
+                <Link href={`/Asociation/${associationCode}/Settings`} className='gap-4'>
 
                     <Button className="w-80 flex items-center justify-center gap-2">
                         <FaCog />
