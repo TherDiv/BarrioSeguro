d22ee143ea9e1ab188f97923a141321413e935c2
Hecze <hectorzerrillo85@gmail.com>
2024-06-30 22:49:35 -0500

    spider ed loading agregado

--- app/Asociation/page.tsx
+++ app/Asociation/page.tsx
@@ -1,21 +1,24 @@
 "use client";
 import { Asociation } from '@/components/Asociation';
 import Link from 'next/link';
+import { Spinner } from "@nextui-org/react";
 import { useEffect } from 'react';
 
 export default function AsociationPage() {
   //redirigir a homr con un useffect
-  
+
   useEffect(() => {
-    window.location.href = '/';
+    //obtener asociation code del local storage
+    const associationCode = localStorage.getItem('associationId');
+    if (!associationCode) {
+      window.location.href = `/`;
+    }
+    window.location.href = `/Asociation/${associationCode}`;
   }, []);
 
   return (
-        <>
-        <h1>
-          Pagina no encontrada, 
-        </h1>
-        <Link className='text-blue-500 underline ' href="/">volver a la  página principal</Link>
-        </>
+    <>
+      <Spinner size='lg' />
+    </>
   );
 }
